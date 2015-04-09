4460e79af6d06fe10079fd634f19973c638d78b5
TanPhan <tanitct89@gmail.com>
2015-04-09 10:39:32 +0700

    convert to angular blog

--- js/app.js
+++ js/app.js
@@ -1,26 +1,2 @@
 ' use strict';
-/* App Module */
-var helloWorldApp = angular.module('helloWorldApp', [
-    'ngRoute',
-    'helloWorldControllers'
-]);
-helloWorldApp.config(['$routeProvider', '$locationProvider',
-    function ($routeProvider, $locationProvider) {
-        $routeProvider.when('/', {
-            templateUrl: 'template/main.html',
-            controller: 'MainCtrl'
-        }).when('/show', {
-            templateUrl: 'template/show.html',
-            controller: 'ShowCtrl'
-        }).when('/customer', {
-            templateUrl: 'template/customer.html',
-            controller: 'CustomerCtrl'
-        }).when('/addCustomer', {
-            templateUrl: 'template/newCustomer.html',
-            controller: 'AddCustomerCtrl'
-        }).when('/addedCustomer/:customer/:city', {
-            templateUrl: 'template/addedCustomer.html',
-            controller: 'AddedCustomerCtrl'
-        });
-        $locationProvider.html5Mode(false).hashPrefix('!');
-    }]);
+var blogApp = angular.module('blogApp', ['ngRoute', 'blogControllers']);
