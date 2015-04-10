6a6e3c03dd83e310ecff3f0295c455999dfa325a
TanPhan <tanitct89@gmail.com>
2015-04-10 09:59:21 +0700

    blog index

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 ' use strict';
 var blogApp = angular.module('blogApp', ['ngRoute', 'blogControllers']);
+blogApp.config(['$routeProvider', '$locationProvider',
+function($routeProvider, $locationProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'template/main.html',
+        controller: 'BlogCtrl'
+    });
+    $locationProvider.html5Mode(false).hashPrefix('!');
+}]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,9 @@
 ' use strict';
+var blogControllers = angular.module('blogControllers', []);
+blogControllers.controller('BlogCtrl',
+    ['$scope',
+        function BlorCtrl($scope) {
+            $scope.blogArticle = 'Blog Test';
+
+        }
+    ]);
