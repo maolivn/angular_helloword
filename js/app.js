ecfdc457965b92816075736ea6fab062dd839588
TanPhan <tanitct89@gmail.com>
2015-04-10 15:11:24 +0700

    add blog post

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ function($routeProvider, $locationProvider){
     $routeProvider.when('/', {
         templateUrl: 'template/main.html',
         controller: 'BlogCtrl'
+    }).when('/blogPost/:id', {
+        templateUrl: 'template/blogPost.html',
+        controller: 'BlogViewCtrl'
     });
     $locationProvider.html5Mode(false).hashPrefix('!');
 }]);
